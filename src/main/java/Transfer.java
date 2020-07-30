58ac06eed1cafe97a36127ca6f0493debe306581
glebka191 <60113335+glebka191@users.noreply.github.com>
2020-07-30 12:51:24 +0300

    Update Transfer.java

--- src/main/java/Transfer.java
+++ src/main/java/Transfer.java
@@ -5,7 +5,6 @@ import java.util.Map;
 public class Transfer {
     public static void main(String[] args) throws IOException, Distribution.ValidationException {
         BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
         while (reader.ready()) {
             String allLine = reader.readLine();
             if (!allLine.equals("")) {
@@ -22,16 +21,10 @@ public class Transfer {
         }
         System.out.println(Distribution.result.toString());
 
-        try {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]))){
             writer.write(Distribution.result.toString());
-        }
-        catch (IOException e){
+        } catch (IOException e){
             System.out.println("Ошибка при записи");
-        }
-        finally {
-            writer.flush();
-            writer.close();
-            reader.close();
         }
     }
 }
